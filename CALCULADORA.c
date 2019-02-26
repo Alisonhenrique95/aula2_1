b7d1eeab73ee6c64f154965a7af1400375ea7b05
Alisonhenrique95 <47761957+Alisonhenrique95@users.noreply.github.com>
2019-02-25 22:14:00 -0300

    Add files via upload

---
+++ CALCULADORA.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+// Programa 'le e imprime um valor'
+int main()
+{
+	int A=0;
+	int B=0;
+	
+	printf("informe um valor A inteiro: \n");
+	
+	scanf ("%i",&A);
+	
+	printf("O valor de A e: %i\n",A);
+	
+	printf("informe um valor B inteiro: \n");
+	
+	scanf("%i",&B);
+	
+	printf("O valor de B e: %i\n",B);
+	
+	printf ("soma : %i \n",A + B);
+	
+	printf ("substracao: %i \n", A - B);
+	
+	printf ("Multiplicacao: %i \n", A * B);
+	
+	printf ("Divisao: %i \n", A / B);
+	
+	
+	
+
+	
+}
